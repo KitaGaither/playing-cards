ff32a714a48de61ab33af845b19d88784840c72b
KitaGaither <lakitgai@amazon.com>
2022-06-22 09:29:16 -0700

    Moved comparator to CardTrick.java.

--- src/main/java/com/tlglearning/playingcards/com/tlglearning/playingcards/CardTrick.java
+++ src/main/java/com/tlglearning/playingcards/com/tlglearning/playingcards/CardTrick.java
@@ -1,6 +1,5 @@
 package com.tlglearning.playingcards.com.tlglearning.playingcards;
 
-import com.tlglearning.playingcards.com.util.DisplayComparator;
 import com.tlglearning.playingcards.model.Card;
 import com.tlglearning.playingcards.model.Deck;
 import com.tlglearning.playingcards.model.Suit;
@@ -78,3 +77,15 @@ public class CardTrick {
 
 }
 
+class DisplayComparator implements Comparator<Card> {
+
+    @Override
+    public int compare(Card card1, Card card2) {
+
+        int comparison = card1.getSuit().getColor().compareTo(card2.getSuit().getColor()); //matches return type
+        comparison = (comparison != 0) ? comparison : card1.getSuit().compareTo(card2.getSuit());
+        comparison = (comparison != 0) ? comparison : card1.getRank().compareTo(card2.getRank());
+
+        return comparison; //matches field/local variable
+    }
+}
